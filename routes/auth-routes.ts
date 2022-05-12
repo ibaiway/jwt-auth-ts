c589fdd6286e3e7e5335412d32ec1f24fc5f77b3
ibaialberdi <ibaialberdi@gmail.com>
2022-05-13 01:56:36 +0200

    ADDED: /login endpoint

--- routes/auth-routes.ts
+++ routes/auth-routes.ts
@@ -7,13 +7,13 @@ const authRouter = Router();
 authRouter.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
   const salt = await bcrypt.genSalt(10);
-  const encryptedPassword = await bcrypt.hash(password, salt);
+  const hash = await bcrypt.hash(password, salt);
 
   try {
     const user = await UserModel.create({
       username,
       email,
-      password: encryptedPassword
+      password: hash
     });
     res.json({
       error: null,
@@ -22,6 +22,28 @@ authRouter.post('/register', async (req, res) => {
   } catch (error) {
     res.status(400).json({ error });
   }
+});
+
+authRouter.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    return res.status(400).json({
+      error: 'Incorrect login details'
+    });
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    return res.status(400).json({
+      error: 'Incorrect login details'
+    });
+  }
+
+  res.status(200).json({
+    error: null,
+    data: 'Welcome Home to Barcelona Hackathon'
+  });
 });
 
 export { authRouter };
